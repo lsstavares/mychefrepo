26ebb6bea580401ce762002bad2ff7356ef85f30
Leonardo Tavares <ltavares_99@yahoo.com>
2013-12-12 14:32:21 -0200

    base server

---
+++ cookbooks/base-server-packages/recipes/default.rb
@@ -0,0 +1,74 @@
+#
+# Cookbook Name:: base-server-packages
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# install xbase-clients
+package xbase-clients do 
+	action :install
+end
+
+# install xosview
+package xosview do 
+	action :install
+end
+
+# install less
+package less do 
+	action :install
+end
+
+# install apt-file
+package apt-file do 
+	action :install
+end
+
+# install ntpdate
+package ntpdate do 
+	action :install
+end
+
+# install telnet
+package telnet do 
+	action :install
+end
+
+# install netcat
+package netcat do 
+	action :install
+end
+
+# install psmisc
+package psmisc do 
+	action :install
+end
+
+# install mlocate
+package mlocate do 
+	action :install
+end
+
+# install screen
+package screen do 
+	action :install
+end
+
+# install heirloom-mailx
+package heirloom-mailx do 
+	action :install
+end
+
+# install alpine
+package alpine do 
+	action :install
+end
+
+# install bzip2
+package bzip2 do 
+	action :install
+end
+
